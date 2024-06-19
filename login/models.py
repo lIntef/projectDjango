46d74bcaca4e464f1971e92f0c2c6e2842ce3672
JH-ED-XI <eacorrea382@soy.sena.edu.co>
2024-06-19 04:40:47 -0500

    creacion del superusuario (admin)

--- login/models.py
+++ login/models.py
@@ -1,22 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserProfileManager(BaseUserManager):
-    def create_user(self, numero, username, email, password=None):
+    def create_user(self, numero, username, email, password=None, tipo=2):
         if not numero:
             raise ValueError('Users must have a document number')
+
+        if not email:
+            raise ValueError('Users must have an email address')
 
         user = self.model(
             numero=numero,
             username=username,
-            email=email,
+            email=self.normalize_email(email),
+            tipo=tipo
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-class UserProfile(AbstractBaseUser):
+    def create_superuser(self, numero, username, email, password=None, tipo=2):
+        user = self.create_user(
+            numero=numero,
+            username=username,
+            email=email,
+            password=password,
+            tipo=tipo,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     tipo_choices = (
         (1, 'T.I'),
         (2, 'C.C'),
@@ -24,11 +42,15 @@ class UserProfile(AbstractBaseUser):
         (4, 'C.I'),
     )
 
-    tipo = models.PositiveSmallIntegerField(choices=tipo_choices)
+    tipo = models.PositiveSmallIntegerField(choices=tipo_choices, default=2)
     numero = models.PositiveIntegerField(unique=True)
     username = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
@@ -37,3 +59,13 @@ class UserProfile(AbstractBaseUser):
 
     def __str__(self):
         return str(self.numero)
+
+class UserAddress(models.Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.user.username}'s Address"
