f4ad5b89e5920927541e966470d6e0c39789aa86
JH-ED-XI <eacorrea382@soy.sena.edu.co>
2024-06-20 11:26:13 -0500

    otro poco mas sobre perfil

--- staticfiles/js/dashboardDoc.js
+++ staticfiles/js/dashboardDoc.js
@@ -1,4 +1,4 @@
-let profileDropdownList = document.querySelector(".profile-dropdown-list");
+let profileDropdownList = document.querySelector(".navbar-list");
 let btn = document.querySelector(".profile-dropdown-btn");
 
 let classList = profileDropdownList.classList;
@@ -7,6 +7,41 @@ const toggle = () => classList.toggle("active");
 
 window.addEventListener("click", function (e) {
     if (!btn.contains(e.target)) classList.remove("active");
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const items = document.querySelectorAll('.profile-dropdown-list-item');
+
+  items.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+      // Eliminar la clase hover de todos los items
+      items.forEach(i => i.classList.remove('hover'));
+      // Añadir la clase hover al item actual
+      item.classList.add('hover');
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const items = document.querySelectorAll('.profile-dropdown-list-item');
+
+  items.forEach(item => {
+    let timer;
+    
+    item.addEventListener('mouseenter', () => {
+      clearTimeout(timer);
+      // Añadir la clase hover al item actual
+      item.classList.add('hover');
+    });
+
+    item.addEventListener('mouseleave', () => {
+      // Configurar el temporizador para eliminar la clase hover después de 2 segundos
+      timer = setTimeout(() => {
+        item.classList.remove('hover');
+      }, 800);
+    });
+  });
 });
 
 const profileDropdownBtn = document.querySelector('.profile-dropdown-btn');
