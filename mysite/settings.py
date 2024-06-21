177a8b06ea87283cf8247ad21fcaa7c8a67e0fbb
JH-ED-XI <eacorrea382@soy.sena.edu.co>
2024-06-21 11:30:41 -0500

    build($cambios): cambios estilos de base y direccion de estilos static

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,17 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'login/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'login', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'login/static'),
-]
-STATIC_ROOT = BASE_DIR / 'login/static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 AUTH_USER_MODEL = 'login.UserProfile'
